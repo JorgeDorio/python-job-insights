a040c71b6fc6512e0d7b0750e767566b49e104c7
Jorge Dorio <jorgegabrielbd.jj@gmail.com>
2022-10-20 16:48:25 -0300

    3 - Implemente a função get_unique_industries

--- src/insights.py
+++ src/insights.py
@@ -30,21 +30,14 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
+    data = jobs.read(path)
+    print(data)
+    industries = []
+    for job in data:
+        if job["industry"] != "":
+            industries.append(job["industry"])
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    return list(set(industries))
 
 
 def filter_by_industry(jobs, industry):
