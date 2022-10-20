e93aa0bd9584057c49cb3d717109e41a0b4f5f35
Jorge Dorio <jorgegabrielbd.jj@gmail.com>
2022-10-20 18:08:10 -0300

    6 - Implemente a função filter_by_job_type

--- src/insights.py
+++ src/insights.py
@@ -14,21 +14,8 @@ def get_unique_job_types(path):
 
 
 def filter_by_job_type(jobs, job_type):
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    return []
+    filtered = [i for i in jobs if i["job_type"] == job_type]
+    return filtered
 
 
 def get_unique_industries(path):
